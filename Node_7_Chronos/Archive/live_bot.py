20a69bb29e15229596fb2da6af070d8f63680476
NOVEYA <ngoisigma@gmail.com>
2026-04-08 17:28:31 +0300

    Change API token in live_bot.py
    Updated API token to a new value for security.

--- Node_7_Chronos/Archive/live_bot.py
+++ Node_7_Chronos/Archive/live_bot.py
@@ -27,7 +27,7 @@ except Exception as e:
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # ТВОЙ АКТУАЛЬНЫЙ ТОКЕН
-API_TOKEN = "7960143628:AAFIOo9_YypK2b1oGk3zy4m9Sy7FFfcL48g"
+API_TOKEN = "7--------"
 
 bot = telebot.TeleBot(API_TOKEN)
 translator = Translator()
